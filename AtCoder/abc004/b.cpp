badb20cc7a3ae7ddc0d5b28a7f3541fce0e56742
mi0-1123 <si2.o.mjt@gmail.com>
2018-01-05 16:18:35 +0900

    acv004/b.cpp

---
+++ AtCoder/abc004/b.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    char c[4][4];
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            cin >> c[i][j];
+        }
+    }
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            cout << c[3-i][3-j];
+            if(j!=3)cout << " ";
+        }
+        cout << endl;
+    }
+}
