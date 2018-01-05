f7e913628675ae6cb56b496af65f2beaa9499b00
mi0-1123 <si2.o.mjt@gmail.com>
2018-01-05 14:47:00 +0900

    add abc003/b.cpp

---
+++ AtCoder/abc003/b.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string s,t;
+    cin >> s >> t;
+    string atmk = "atcoder";
+    bool flg = true;
+    for(int i=0;i<s.size();i++){
+        if(s[i]!=t[i]){
+            flg=false;
+            if(s[i]=='@'){
+                for(int j=0;j<atmk.size();j++){
+                    if(t[i]==atmk[j])flg=true;
+                }
+            }else if(t[i]=='@'){
+                for(int j=0;j<atmk.size();j++){
+                    if(s[i]==atmk[j])flg=true;
+                }
+            }
+        }
+    }
+
+    if(flg)cout << "You can win" << endl;
+    else cout << "You will lose" << endl;
+}
