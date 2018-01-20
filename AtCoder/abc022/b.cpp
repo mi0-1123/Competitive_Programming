ff72c64c1fafaae3db55ed34609b817e53e64f0e
mi0-1123 <si2.o.mjt@gmail.com>
2018-01-20 20:25:05 +0900

    add abc022/b.cpp

---
+++ AtCoder/abc022/b.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define Rep(i,n) for(int i=0;i<n;i++)
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> table(100000,0);
+    int a;
+    Rep(i,n){
+        cin >> a;
+        table[a]++;
+    }
+    int sum=0;
+    Rep(i,100000){
+        if(table[i]>1)sum+=table[i]-1;
+    }
+    cout << sum << endl;
+}
