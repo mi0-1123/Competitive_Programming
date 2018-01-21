6171b2366ed715303cdfacc5f612d9e2bde708e1
mi0-1123 <si2.o.mjt@gmail.com>
2018-01-22 00:17:34 +0900

    add tmpfile

---
+++ tempfile/tmp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cstdio>
+#include <map>
+using namespace std;
+
+#define Int long long int
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REAP(i,a,n) for(int i=(a);i<(n);i++)
+#define YES cout<<"Yes"<<endl
+#define NO cout<<"No"<<endl
+#define fr first
+#define sc second
+#define pb push_back
+#define All(v) v.begin(),v.end()
+
