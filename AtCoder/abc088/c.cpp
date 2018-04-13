392d9460dd205d70c783d9241936593e622ec629
mi0-1123 <si2.o.mjt@gmail.com>
2018-04-13 12:21:49 +0900

    fix

--- AtCoder/abc088/c.cpp
+++ AtCoder/abc088/c.cpp
@@ -11,3 +11,24 @@ using namespace std;
 #define pb push_back
 #define All(v) v.begin(),v.end()
 
+    int a[3],b[3];
+    int c[3][3];
+
+int main(){
+    REP(i,3)REP(j,3){
+        cin >> c[i][j];
+    }
+
+    REP(i,3)b[i]=c[0][i];
+
+    a[1]=c[1][0]-b[0];
+    a[2]=c[2][0]-b[0];
+
+    bool flg=true;
+    REP(i,3)REP(j,3){
+        if(a[i]+b[j]!=c[i][j])flg=false;
+    }
+    if(flg)YES;
+    else NO;
+
+}
