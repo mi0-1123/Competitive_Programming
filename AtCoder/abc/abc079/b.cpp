d20886da99955c42307b19c18764f222c1726d34
mi0-1123 <si2.o.mjt@gmail.com>
2018-04-17 12:27:17 +0900

    add

--- AtCoder/abc/abc079/b.cpp
+++ AtCoder/abc/abc079/b.cpp
@@ -1,21 +1,28 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-long long fiv(long long n){
-    if(n<=2){
-        return 1;
-    }else{
-        return fiv(n-2) + fiv(n-1);
+typedef long long ll;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REAP(i,a,n) for(int i=(a);i<(n);i++)
+#define YES cout<<"Yes"<<endl
+#define NO cout<<"No"<<endl
+#define fr first
+#define sc second
+#define pb push_back
+#define All(v) v.begin(),v.end()
+
+ll L[100];
+ll lucas(){
+    REAP(i,2,99){
+        L[i]=L[i-1]+L[i-2];
     }
 }
 
 int main(){
+    L[0]=2;
+    L[1]=1;
     int n;
     cin >> n;
-
-    //long long sum;
-   if(n>1)cout << fiv(n-1)*2 +fiv(n) << endl;
-   else if(n==1)cout << 1 << endl;
-   else cout << 2 << endl;
+    lucas();
+    cout << L[n] << endl;
 }
