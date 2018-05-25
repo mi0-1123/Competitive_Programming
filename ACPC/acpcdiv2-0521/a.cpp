5adf8b5dcd49cb4c9521b4be9ea8f9e128cc65c5
mi0-1123 <si2.o.mjt@gmail.com>
2018-05-25 18:34:31 +0900

    add

---
+++ ACPC/acpcdiv2-0521/a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REAP(i,a,n) for(int i=(a);i<(n);i++)
+#define YES cout<<"Yes"<<endl
+#define NO cout<<"No"<<endl
+#define fr first
+#define sc second
+#define pb push_back
+#define All(v) v.begin(),v.end()
+typedef long long ll;
+
+int main(){
+    ios::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    vector<vector<bool> > a(3,vector<int>(n,false));
+    int xyz;
+    REP(i,3){
+        cin >> xyz;
+        REP(j,xyz){
+            int t;
+            cin >> t;
+            a[i][t-1]=true;
+        }
+    }
+}
